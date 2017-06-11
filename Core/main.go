7e50824363f1c35bd8dae9eada12406624088cf5
Gurpreet Singh <gsingh@ca.ibm.com>
2017-06-11 18:57:41 -0400

    Implementing simple service to learn Go and GoKit

--- Core/main.go
+++ Core/main.go
@@ -20,4 +20,47 @@ type StringService interface
     Count(string) int
 }
 
+type stringService struct{}
+
+//This is an implementation for stringService. Takes in a String, returns a tuple?
+func (stringService) Uppercase(input String) (string, error)
+{
+    //Another language that does string comparisons with ==.
+    if input == ""
+    {
+        return "", ErrEmpty
+    }
+
+    return strings.ToUpper(s), nil
+}
+
+//Another implementation for stringService. Returns int
+func (stringService) Count(input String) int
+{
+    return len(input)
+}
+
+//We need request and response structs for each call. Not sure why... yet
+type uppercaseRequest struct
+{
+    S string `json:"s"`
+}
+
+type uppercaseResponse struct
+{
+    V string `json:"v"`
+    Err string `json:"err, omitempty"`
+}
+
+type countRequest struct
+{
+    S string `json:"s"`
+}
+
+type countResponse struct
+{
+    V int `json:"v"`
+}
+
+
 
