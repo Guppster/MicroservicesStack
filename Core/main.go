481ddde1c9fb875647d02a93f04febcd6a166eee
Gurpreet Singh <gsingh@ca.ibm.com>
2017-06-12 22:28:38 -0400

    GO ENFORCED BAD BRACES. IM DONE. NEVER WRITING GO AGAIN. ABSOLUTELY FILTHY

--- Core/main.go
+++ Core/main.go
@@ -3,20 +3,18 @@ package main
 import
 (
     "context"
-    "encoders/json"
-    "errors"
+    "encoding/json"
     "log"
+    "errors"
     "net/http"
     "strings"
 
     "github.com/go-kit/kit/endpoint"
     httptransport "github.com/go-kit/kit/transport/http"
-
 )
 
 //Main function
-func main()
-{
+func main() {
     service := stringService{}
 
     uppercaseHandler := httptransport.NewServer(
@@ -36,64 +34,60 @@ func main()
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func decodeUppercaseRequest(context context.Context, req *http.Request) (interface{}, error)
-{
+func decodeUppercaseRequest(context context.Context, req *http.Request) (interface{}, error){
     var request uppercaseRequest
 
-    if err := json.NewDecoder(req.Body).Decode(&request); err != nil
-    {
+    if err := json.NewDecoder(req.Body).Decode(&request); err != nil{
         return nil, err
     }
 
     return request, nil
 }
 
-func decodeCountRequest(context context.Context, req *http.Request) (interface{}. error)
-{
+func decodeCountRequest(context context.Context, req *http.Request) (interface{}, error) {
     var request countRequest
 
-    if err := json.NewDecoder(req.Body).Decode(&request); err != nil
-    {
+    if err := json.NewDecoder(req.Body).Decode(&request); err != nil{
         return nil, err
     }
 
     return request, nil
 }
 
-func encodeResponse(context context.Context, writer http.ResponseWriter, response interface{}) error
-{
+func encodeResponse(context context.Context, writer http.ResponseWriter, response interface{}) error {
     return json.NewEncoder(writer).Encode(response)
 }
 
 //Returns an endpoint for converting to uppercase
-func makeUppercaseEndpoint(service StringService) endpoint.Endpoint
-{
-    return func(context context.Context, request interface{}) (interface{}, error)
-    {
+func makeUppercaseEndpoint(service StringService) endpoint.Endpoint{
+
+    return func(context context.Context, request interface{}) (interface{}, error) {
+
         req := request.(uppercaseRequest)
         v, err := service.Uppercase(req.S)
-        if err != nil
-        {
-            return uppercaseResponse{v, err.Error{}}, nil
+
+        if err != nil {
+            return uppercaseResponse{v, err.Error()}, nil
         }
+
         return uppercaseResponse{v, ""}, nil
     }
 }
 
 //Returns an endpoint for counting characters
-func makeCountEndpoint(service StringService) endpoint.Endpoint
-{
-    return func(context context.Context, request interface{}) (interface{}. error)
-    {
+func makeCountEndpoint(service StringService) endpoint.Endpoint{
+
+    return func(context context.Context, request interface{}) (interface{}, error){
+
         req := request.(countRequest)
         v := service.Count(req.S)
         return countResponse{v}, nil
+
     }
 }
 
 //StringService provides operations on strings
-type StringService interface
-{
+type StringService interface{
     Uppercase(string) (string, error)
     Count(string) int
 }
@@ -101,25 +95,26 @@ type StringService interface
 type stringService struct{}
 
 //This is an implementation for stringService. Takes in a String, returns a tuple?
-func (stringService) Uppercase(input String) (string, error)
-{
+func (stringService) Uppercase(input string) (string, error) {
+
     //Another language that does string comparisons with ==.
-    if input == ""
-    {
+    if input == ""{
         return "", ErrEmpty
     }
 
-    return strings.ToUpper(s), nil
+    return strings.ToUpper(input), nil
 }
 
 //Another implementation for stringService. Returns int
-func (stringService) Count(input String) int
-{
+func (stringService) Count(input string) int{
     return len(input)
 }
+
+// ErrEmpty is returned when input string is empty
+var ErrEmpty = errors.New("Empty string")
+
 //We need request and response structs for each call. Not sure why... yet
-type uppercaseRequest struct
-{
+type uppercaseRequest struct{
     S string `json:"s"`
 }
 
@@ -138,6 +133,4 @@ type countResponse struct
 {
     V int `json:"v"`
 }
-
-
 
